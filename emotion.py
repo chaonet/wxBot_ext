3db5c476d30f21dffc9a2fe8e428b3add4088724
chaonet <xuchaorfc@gmail.com>
2016-03-31 23:03:18 +0800

    return emotion

--- emotion.py
+++ emotion.py
@@ -5,15 +5,68 @@ import os
 import httplib, urllib, base64
 from wxbot import *
 
+import ConfigParser
+
 class Emotion_api(WXBot):
     
     def __init__(self):
         WXBot.__init__(self)
-        self.emotion_key = os.environ.get("emotion_key") or 'NULL'
+        if os.path.exists('conf.ini'):
+            cf = ConfigParser.ConfigParser()
+            cf.read('conf.ini')
+            self.emotion_key = cf.get('main', 'emotion_key')
+        else:
+            self.emotion_key = os.environ.get("emotion_key") or 'NULL'
         self.robot_switch = False
+        print self.emotion_key
+
+        self.headers = {
+        # Request headers
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': self.emotion_key ,
+        }
+
+        self.data = {'url': ''}
+        self.body = ''
     
     def emotion_api(self, uid, url):
-        return url
+        if self.emotion_key == 'NULL':
+            return
+        self.data['url'] = str(url)
+        # https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxgetmsgimg?MsgID=5544385610890911816&skey=@crypt_115bea6f_8dc493994755faf3c3f903e5cd66ece4
+        print url,1
+        print self.data['url']
+        print type(url)
+        # <type 'unicode'>
+        print type(self.data['url'])
+        # <type 'str'>
+
+        # self.data['url'] = 'http://t1.qpic.cn/mblogpic/c4e7677ac8cb9a4068f6/160'
+        self.body = json.dumps(self.data)
+
+        try:
+            conn = httplib.HTTPSConnection('api.projectoxford.ai')
+            conn.request("POST", "/emotion/v1.0/recognize", self.body, self.headers)
+            response = conn.getresponse()
+            data = json.loads(response.read())
+            # print data[0]['scores']
+            print data, 1
+            # {u'error': {u'message': u'Image size is too small or too big.', u'code': u'InvalidImageSize'}} 1
+
+            emotion_list = [u'sadness', u'neutral', u'contempt', u'disgust', u'anger', u'surprise', u'fear', u'happiness']
+            emotion_max = ''
+            max_value = 0
+            for i in emotion_list:
+                if data[0]['scores'][i] > max_value:
+                    emotion_max = i
+                    max_value = data[0]['scores'][i]
+
+            conn.close()
+        except Exception as e:
+            print 2
+            print e
+        emotion_dir = {u'sadness': u'很悲伤', u'neutral': u'不动声色', u'contempt': u'轻蔑', u'disgust': u'厌恶', u'anger': u'生气', u'surprise': u'惊讶', u'fear': u'害怕', u'happiness': u'开心'}
+        return emotion_dir[emotion_max]
 
     def auto_switch(self, msg):
         msg_data = msg['content']['data']
